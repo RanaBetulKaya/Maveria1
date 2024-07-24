ddaee90de54af800c36e95989381168d89180de9
RanaBetulKaya <rana20032016@icloud.com>
2024-07-24 13:40:18 +0300

    Pytorch image

---
+++ Pytorch/app.py
@@ -0,0 +1,11 @@
+import torch
+import pandas as pd
+import numpy as np
+
+print(f"PyTorch versiyonu: {torch.__version__}")
+print("Pandas version:", pd.__version__)
+print("Numpy version:", np.__version__)
+
+
+print("Rana")
+
