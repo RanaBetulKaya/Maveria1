dd5bc5b0e08dbb0cf97a96a1697858b19996e73d
RanaBetulKaya <rana20032016@icloud.com>
2024-07-25 09:18:14 +0300

    Tensorflow image

---
+++ Tensorflow/app.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+print("Tensorflow version:", tf.__version__)
+print("Pandas version:", pd.__version__)
+print("Numpy version:", np.__version__)
+
+
+print("Rana")
